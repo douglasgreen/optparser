f63b415a09eb408825bb09e3105b947a13488933
Douglas S. Green <douglas@nurd.site>
2024-05-05 12:05:58 -0400

    Adding commands

--- src/OptionHandler.php
+++ src/OptionHandler.php
@@ -18,6 +18,17 @@ class OptionHandler
     /** @var array<string, bool> */
     protected $allAliases = [];
 
+    /** @var array<string, bool> */
+    protected $allNames = [];
+
+    /**
+     * @var array<string, array{
+     *     aliases: list<string>,
+     *     desc: string
+     * }>
+     */
+    protected $commands = [];
+
     /**
      * @var array<string, array{
      *     aliases: list<string>,
@@ -36,26 +47,35 @@ class OptionHandler
     protected $params = [];
 
     /**
-     * @var array<string, string>
+     * @var array<string, array{
+     *      type: string,
+     *      desc: string
+     * }>
      */
     protected $terms = [];
 
     /**
+     * A command is a predefined list of command words.
+     *
+     * @param list<string> $aliases
+     */
+    public function addCommand(array $aliases, string $desc): void
+    {
+        [$name, $others] = $this->pickName($aliases);
+        $this->commands[$name] = [
+            'aliases' => $others,
+            'desc' => $desc
+        ];
+    }
+
+    /**
      * A flag has no arguments.
      *
-     * @param array<string> $aliases
+     * @param list<string> $aliases
      */
     public function addFlag(array $aliases, string $desc): void
     {
-        $name = $this->pickName($aliases);
-        $this->checkAlias($name);
-        $others = [];
-        foreach ($aliases as $alias) {
-            if ($alias != $name) {
-                $this->checkAlias($alias);
-                $others[] = $alias;
-            }
-        }
+        [$name, $others] = $this->pickName($aliases);
         $this->flags[$name] = [
             'aliases' => $others,
             'desc' => $desc
@@ -65,22 +85,14 @@ class OptionHandler
     /**
      * A parameter has a required argument.
      *
-     * @param array<string> $aliases
+     * @param list<string> $aliases
      */
     public function addParam(
         array $aliases,
         string $type,
         string $desc
     ): void {
-        $name = $this->pickName($aliases);
-        $this->checkAlias($name);
-        $others = [];
-        foreach ($aliases as $alias) {
-            if ($alias != $name) {
-                $this->checkAlias($alias);
-                $others[] = $alias;
-            }
-        }
+        [$name, $others] = $this->pickName($aliases);
         $type = strtoupper($type);
         $this->checkType($type);
         $this->params[$name] = [
@@ -93,28 +105,76 @@ class OptionHandler
     /**
      * A term is a positional argument.
      */
-    public function addTerm(string $name, string $desc): void
+    public function addTerm(string $name, string $type, string $desc): void
     {
         $this->checkAlias($name);
-        $this->terms[$name] = $desc;
+        $this->allNames[$name] = true;
+        $this->terms[$name] = [
+            'desc' => $desc,
+            'type' => $type
+        ];
     }
 
     /**
-     * Print a list of options, arguments, and descriptions.
+     * Has the name been defined?
      */
-    public function printOptions(): void
+    public function hasName(string $name): bool
     {
+        return isset($this->names[$name]);
+    }
+
+    /**
+     * Write an option by name.
+     *
+     * @throws OptionParserException
+     */
+    public function writeOption(string $name): string
+    {
+        if (isset($this->commands[$name])) {
+            return $name;
+        }
+
+        if (isset($this->params[$name])) {
+            $param = $this->params[$name];
+            return $this->hyphenate($name) . '=' . $param['type'];
+        }
+
+        if (isset($this->terms[$name])) {
+            $term = $this->terms[$name];
+            return $this->hyphenate($name) . ':' . $term['type'];
+        }
+
+        if (isset($this->flags[$name])) {
+            return $this->hyphenate($name);
+        }
+
+        throw new OptionParserException("Name not found");
+    }
+
+    /**
+     * Get a string representation of options, arguments, and descriptions.
+     */
+    public function writeOptionBlock(): string
+    {
+        $output = '';
+
+        if ($this->commands) {
+            $output .= $this->writeCommandBlock();
+        }
+
         if ($this->params) {
-            $this->printParams();
+            $output .= $this->writeParamBlock();
         }
 
         if ($this->flags) {
-            $this->printFlags();
+            $output .= $this->writeFlagBlock();
         }
 
         if ($this->terms) {
-            $this->printTerms();
+            $output .= $this->writeTermBlock();
         }
+
+        return $output;
     }
 
     /**
@@ -142,16 +202,31 @@ class OptionHandler
         }
     }
 
+    protected function hyphenate(string $alias): string
+    {
+        if (strlen($alias) == 1) {
+            return '-' . $alias;
+        } else {
+            return '--' . $alias;
+        }
+    }
+
     /**
-     * @param array<string> $aliases
+     * @param list<string> $aliases
+     * @return array{string, list<string>}
      * @throws OptionParserException
      */
-    protected function pickName(array $aliases): string
+    protected function pickName(array $aliases): array
     {
         $name = null;
+        $others = [];
         foreach ($aliases as $alias) {
+            $this->checkAlias($alias);
             if (!$name && strlen($alias) > 1) {
                 $name = $alias;
+                $this->allNames[$name] = true;
+            } else {
+                $others[] = $alias;
             }
         }
 
@@ -159,57 +234,63 @@ class OptionHandler
             throw new OptionParserException("Missing required long name");
         }
 
-        return $name;
+        return [$name, $others];
     }
 
-    protected function printAlias(string $alias): void
+    protected function writeCommandBlock(): string
     {
-        if (strlen($alias) == 1) {
-            echo '-' . $alias;
-        } else {
-            echo '--' . $alias;
+        $output = "Commands:\n";
+        foreach ($this->commands as $name => $command) {
+            $output .= "  $name";
+            foreach ($command['aliases'] as $alias) {
+                $output .= " | $alias";
+            }
+            $output .= '  ' . $command['desc'] . "\n";
         }
+        $output .= "\n";
+        return $output;
     }
 
-    protected function printFlags(): void
+    protected function writeFlagBlock(): string
     {
-        echo "Flags:\n";
+        $output = "Flags:\n";
         foreach ($this->flags as $name => $flag) {
-            echo '  ';
-            $this->printAlias($name);
+            $output .= '  ';
+            $output .= $this->hyphenate($name);
             foreach ($flag['aliases'] as $alias) {
-                echo ' | ';
-                $this->printAlias($alias);
+                $output .= ' | ';
+                $output .= $this->hyphenate($alias);
             }
-            echo '  ' . $flag['desc'] . "\n";
+            $output .= '  ' . $flag['desc'] . "\n";
         }
-        echo "\n";
+        $output .= "\n";
+        return $output;
     }
 
-    protected function printParams(): void
+    protected function writeParamBlock(): string
     {
-        echo "Parameters:\n";
+        $output = "Parameters:\n";
         foreach ($this->params as $name => $param) {
-            echo '  ';
-            $this->printAlias($name);
+            $output .= '  ';
+            $output .= $this->hyphenate($name);
             foreach ($param['aliases'] as $alias) {
-                echo ' | ';
-                $this->printAlias($alias);
+                $output .= ' | ';
+                $output .= $this->hyphenate($alias);
             }
-            echo ' = ' . $param['type'];
-            echo '  ' . $param['desc'] . "\n";
+            $output .= ' = ' . $param['type'];
+            $output .= '  ' . $param['desc'] . "\n";
         }
-        echo "\n";
+        $output .= "\n";
+        return $output;
     }
 
-    protected function printTerms(): void
+    protected function writeTermBlock(): string
     {
-        if ($this->terms) {
-            echo "Terms:\n";
-            foreach ($this->terms as $name => $desc) {
-                echo "  $name  $desc\n";
-            }
-            echo "\n";
+        $output = "Terms:\n";
+        foreach ($this->terms as $name => $term) {
+            $output .= "  $name: " . $term['type'] . '  ' . $term['desc'] . "\n";
         }
+        $output .= "\n";
+        return $output;
     }
 }
