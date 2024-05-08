5676a1a17fc934ef311ba7db74c84b303ebce3d1
Douglas S. Green <douglas@nurd.site>
2024-05-08 19:04:18 -0400

    Adding Husky

--- tests/ArgumentParserTest.php
+++ tests/ArgumentParserTest.php
@@ -43,49 +43,4 @@ class ArgumentParserTest extends TestCase
 
         $this->assertSame($expected, $argumentParser->getUnmarkedOptions());
     }
-
-    public function testSplitArrayAroundDash(): void
-    {
-        $argv = ['script.php', '-a', '-b', 'arg1', '--foo=bar', '--baz', 'arg2', '--', 'non1', 'non2'];
-        $others = array_slice($argv, 1);
-        $argumentParser = new ArgumentParser($argv);
-
-        $expectedBefore = ['-a', '-b', 'arg1', '--foo=bar', '--baz', 'arg2'];
-        $expectedAfter = ['non1', 'non2'];
-
-        $result = $this->invokeMethod($argumentParser, 'splitArrayAroundDash', [$others]);
-
-        $this->assertSame($expectedBefore, $result[0]);
-        $this->assertSame($expectedAfter, $result[1]);
-    }
-
-    public function testJoinArguments(): void
-    {
-        $argv = ['-abc', '-d', 'arg1', '--foo', 'bar', '--baz=qux'];
-        $argumentParser = new ArgumentParser($argv);
-
-        $expected = ['-a', '-b', '-c', '-d=arg1', '--foo=bar', '--baz=qux'];
-
-        $result = $this->invokeMethod($argumentParser, 'joinArguments', [$argv]);
-
-        $this->assertSame($expected, $result);
-    }
-
-    /**
-     * Call protected/private method of a class.
-     *
-     * @param object $object     Instantiated object that we will run method on
-     * @param string $methodName Method name to call
-     * @param array  $parameters Array of parameters to pass into method
-     *
-     * @return mixed method return
-     */
-    protected function invokeMethod(object $object, string $methodName, array $parameters = [])
-    {
-        $reflectionClass = new \ReflectionClass($object::class);
-        $method = $reflectionClass->getMethod($methodName);
-        $method->setAccessible(true);
-
-        return $method->invokeArgs($object, $parameters);
-    }
 }
