a77c12a239b6ecd1a3465441c89e264ef8a92811
Douglas S. Green <douglas@nurd.site>
2024-05-24 19:36:09 -0400

    feat: using outfile in example

--- bin/sample_usage.php
+++ bin/sample_usage.php
@@ -16,15 +16,15 @@ $optParser->addCommand(['add', 'a'], 'Add a new user')
     ->addCommand(['list', 'l'], 'List all users')
 
     // Adding terms
-    ->addTerm('username', 'string', 'Username of the user')
-    ->addTerm('email', 'string', 'Email of the user')
+    ->addTerm('username', 'STRING', 'Username of the user')
+    ->addTerm('email', 'STRING', 'Email of the user')
 
     // Adding flags
     ->addFlag(['v', 'verbose'], 'Enable verbose output')
     ->addFlag(['q', 'quiet'], 'Suppress output')
 
     // Adding parameters
-    ->addParam(['p', 'password'], 'string', 'Password for the user')
+    ->addParam(['p', 'password'], 'STRING', 'Password for the user')
     ->addParam(
         ['r', 'role'],
         'string',
@@ -39,7 +39,7 @@ $optParser->addCommand(['add', 'a'], 'Add a new user')
             throw new \InvalidArgumentException('Role must be admin, manager, or user');
         }
     )
-    ->addParam(['o', 'output'], 'string', 'Output file for the list command')
+    ->addParam(['o', 'output'], 'OUTFILE', 'Output file for the list command')
 
     // Adding usage examples
     ->addUsage(['add', 'username', 'email', 'password', 'role'])
